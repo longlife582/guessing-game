43d9026d807b8775a983217039052a9d62a8cbf0
longlife582 <promiseoviawele4@gmail.com>
2023-08-02 22:40:33 +0100

    bug fixed

--- index.js
+++ index.js
@@ -6,15 +6,13 @@ const hint = document.querySelector(".hint");
 let inputCount = 0;
 const maxInput = 5;
 let randomNumber = Math.floor(Math.random() * 10) + 1;
-// console.log(randomNumber);
 
 form.addEventListener("submit", (e) => {
   e.preventDefault();
 
-  if(input.value > 10){
-    message.textContent= "please your number can not be greater than 10"
-    return
-
+  if (input.value > 10) {
+    message.textContent = "Please, your number cannot be greater than 10.";
+    return;
   }
 
   if (inputCount < maxInput) {
@@ -24,8 +22,8 @@ form.addEventListener("submit", (e) => {
     if (!isNaN(userGuess)) {
       if (randomNumber === userGuess) {
         message.textContent = `Congratulations! You guessed right. ${randomNumber}`;
+        hint.textContent = `You got it in ${inputCount} attempt(s).`;
         randomNumber = Math.floor(Math.random() * 10) + 1;
-        // console.log(randomNumber);
         inputCount = 0;
       } else {
         message.textContent = `Wrong guess. Try again`;
@@ -34,7 +32,7 @@ form.addEventListener("submit", (e) => {
           hint.textContent = "Your number is too low";
         } else {
           hint.textContent = "Your number is too high";
-        }
+        } 
       }
     } else {
       message.textContent = `Please enter a valid number.`;
@@ -43,9 +41,7 @@ form.addEventListener("submit", (e) => {
     message.textContent = `You've reached the maximum number of attempts. The correct number was ${randomNumber}.`;
     hint.textContent = `You've used all ${maxInput} attempts.`;
     randomNumber = Math.floor(Math.random() * 10) + 1;
-    // console.log(randomNumber);
     inputCount = 0;
-    
   }
   form.reset();
 });
